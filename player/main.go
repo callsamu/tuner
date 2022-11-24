1576387288b11edff02b66ce4f97cb1bbac7565f
callsamu <samuelbree@gmail.com>
2022-11-24 14:59:56 -0300

    observe for pause property changes

--- player/main.go
+++ player/main.go
@@ -71,6 +71,9 @@ func Initialize() {
 	err = MpvInstance.ObserveProperty(0, "loop-playlist", mpv.FORMAT_FLAG)
 	utils.HandleError(err, "Cannot observer loop-playlist property")
 
+	err = MpvInstance.ObserveProperty(0, "pause", mpv.FORMAT_FLAG)
+	utils.HandleError(err, "Cannot observer pause property")
+
 	// start event listener
 	startEventHandler()
 
